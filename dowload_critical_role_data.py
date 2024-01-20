d04021b2988666671a44ecf46137c3c6bdc74e4c
VictorHerrig <github.mhms0@simplelogin.com>
2024-01-19 17:44:36 -0800

    Moving script to critical_role_summarization repo

--- dowload_critical_role_data.py
+++
@@ -1,9 +0,0 @@
-from vctube import VCtube
-
-
-def main():
-    yt_dl = VCtube(output_dir='downloaded_dataset', youtube_url='https://www.youtube.com/watch?v=kpkCcb--r90', lang='en')
-
-
-if __name__ == '__main__':
-    main()
